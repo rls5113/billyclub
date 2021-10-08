4f3a75f0e5b42514ea20b4bc72c10215974479df
rls5113 <stuartrl@comcast.net>
2021-10-07 19:36:30 -0500

    refactor UI points-event-list

--- src/main/angular-client/src/app/app.module.ts
+++ src/main/angular-client/src/app/app.module.ts
@@ -2,16 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { PointsEventComponent } from './points-event/points-event.component';
 import { PlayerComponent } from './player/player.component';
 import { HeaderComponent } from './header/header.component';
-import { PointsEventListComponent } from './points-event/points-event-list/points-event-list.component';
+import { PointsEventListComponent } from './points-event-list/points-event-list.component';
 import {HttpClientModule} from "@angular/common/http";
+import {PointsEventService} from "./points-event-list/points-event.service";
 
 @NgModule({
   declarations: [
     AppComponent,
-    PointsEventComponent,
     PlayerComponent,
     HeaderComponent,
     PointsEventListComponent
@@ -20,7 +19,7 @@ import {HttpClientModule} from "@angular/common/http";
     BrowserModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [PointsEventService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
