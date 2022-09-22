566c57bd71b636b3dd5b540b2e811201f7c1e892
rls5113 <stuartrl@comcast.net>
2022-09-22 13:08:17 -0500

    add start time

--- src/main/java/com/billyclub/points/data/LoadDatabase.java
+++ src/main/java/com/billyclub/points/data/LoadDatabase.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Configuration
@@ -22,10 +23,10 @@ class LoadDatabase {
     CommandLineRunner initDatabase(PointsEventRepository repository) {
         System.out.println("loading data");
         return args -> {
-            log.info("Preloading " + repository.save(new PointsEvent(LocalDateTime.now().plusDays(7).withHour(6).withMinute(0).withSecond(0).withNano(0),2)));
-            log.info("Preloading " + repository.save(new PointsEvent(LocalDateTime.now().plusDays(8).withHour(6).withMinute(0).withSecond(0).withNano(0),3)));
-            log.info("Preloading " + repository.save(new PointsEvent(LocalDateTime.now().plusDays(14).withHour(6).withMinute(0).withSecond(0).withNano(0),4)));
-            log.info("Preloading " + repository.save(new PointsEvent(LocalDateTime.now().plusDays(15).withHour(6).withMinute(0).withSecond(0).withNano(0),3)));
+            log.info("Preloading " + repository.save(new PointsEvent(LocalDate.now().plusDays(7),2)));
+            log.info("Preloading " + repository.save(new PointsEvent(LocalDate.now().plusDays(8),3)));
+            log.info("Preloading " + repository.save(new PointsEvent(LocalDate.now().plusDays(14),4)));
+            log.info("Preloading " + repository.save(new PointsEvent(LocalDate.now().plusDays(15),3)));
         };
     }
 }
--- src/main/java/com/billyclub/points/model/PointsEvent.java
+++ src/main/java/com/billyclub/points/model/PointsEvent.java
@@ -5,7 +5,10 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 @Entity
 @Data
 @AllArgsConstructor
@@ -17,9 +20,10 @@ public class PointsEvent {
     @SequenceGenerator(name="pointsevent_id_seq", sequenceName = "pointsevent_seq", allocationSize = 1, initialValue = 1000)
     @Column(name="id",updatable = false, nullable = false)
     private long id;
-    private LocalDateTime eventDate;
+    private LocalDate eventDate;
+    private LocalTime startTime;
     private int numOfTimes;
-    public PointsEvent(LocalDateTime ldt, int numOfTimes) {
+    public PointsEvent(LocalDate ldt, int numOfTimes) {
         this.eventDate = ldt;
         this.numOfTimes = numOfTimes;
     }
