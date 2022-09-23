ff4762857009159b28ed45b2184355ea21f6bb5a
rls5113 <stuartrl@comcast.net>
2022-09-23 15:09:08 -0500

    points event requestbody validation

--- src/main/java/com/billyclub/points/controller/PointsEventController.java
+++ src/main/java/com/billyclub/points/controller/PointsEventController.java
@@ -1,17 +1,25 @@
 package com.billyclub.points.controller;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 import com.billyclub.points.exceptions.ResourceNotFoundException;
 import com.billyclub.points.model.PointsEvent;
 import com.billyclub.points.model.assembler.PointsEventModelAssembler;
 import com.billyclub.points.model.exceptions.PointsEventNotFoundException;
 import com.billyclub.points.service.PointsEventService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.IanaLinkRelations;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("api/v1")
@@ -33,33 +41,49 @@ public class PointsEventController {
     }
 
     @GetMapping("/pointsEvents")
-    public ResponseEntity<List<PointsEvent>> getAll() {
-        return new ResponseEntity<>(pointsEventService.getAll(), HttpStatus.OK);
+    public CollectionModel<EntityModel<PointsEvent>> getAll() {
+        List<EntityModel<PointsEvent>> events = pointsEventService.getAll().stream()
+                .map(assembler::toModel)
+                .collect(Collectors.toList());
+        return CollectionModel.of(events,
+                linkTo(methodOn(PointsEventController.class).getAll()).withSelfRel());
     }
+//    public ResponseEntity<List<PointsEvent>> getAll() {
+//        return new ResponseEntity<>(pointsEventService.getAll(), HttpStatus.OK);
+//    }
 
-    @PostMapping("/pointsEvents")
-    public ResponseEntity<PointsEvent> add(@RequestBody PointsEvent newPointsEvent) {
-        return new ResponseEntity<>(pointsEventService.save(newPointsEvent), HttpStatus.CREATED);
+    @PostMapping(value = "/pointsEvents",
+            consumes = {MediaType.APPLICATION_XML_VALUE,MediaType.APPLICATION_JSON_VALUE},
+            produces ={MediaType.APPLICATION_XML_VALUE,MediaType.APPLICATION_JSON_VALUE} )
+    @ResponseStatus(HttpStatus.CREATED)
+    public EntityModel<?> add(@Valid @RequestBody PointsEvent newEvent) {
+        return assembler
+                .toModel(pointsEventService.save(newEvent));
     }
     @PutMapping("/pointsEvents/{id}")
-    public ResponseEntity<PointsEvent> update(@PathVariable Long id,@RequestBody PointsEvent body) throws ResourceNotFoundException {
+    public EntityModel<?> update(@PathVariable Long id, @RequestBody PointsEvent body) throws PointsEventNotFoundException {
         PointsEvent event = pointsEventService.findById(id);
         event.setEventDate(body.getEventDate());
         event.setStartTime(body.getStartTime());
         event.setNumOfTimes(body.getNumOfTimes());
-        final PointsEvent updatedEvent = pointsEventService.save(event);
-
-        return ResponseEntity.ok(updatedEvent);
+        final PointsEvent updated = pointsEventService.save(event);
+        return assembler.toModel(updated);
     }
 
     @DeleteMapping("/pointsEvents/{id}")
-    public ResponseEntity<?> delete(@PathVariable Long id) throws PointsEventNotFoundException {
+    public EntityModel<?> delete(@PathVariable Long id) throws PointsEventNotFoundException {
+        PointsEvent toDelete = pointsEventService.findById(id);
         pointsEventService.delete(id);
-        return new ResponseEntity<>(HttpStatus.OK);
+        return assembler.toModel(toDelete);
     }
 
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    private void eventNotFoundHandler(PointsEventNotFoundException ex) {
+
+    }
 //    @ExceptionHandler
-//    @ResponseStatus(HttpStatus.NOT_FOUND)
+//    @ResponseStatus(HttpStatus.BAD_REQUEST)
 //    private void eventNotFoundHandler(PointsEventNotFoundException ex) {
 //
 //    }
--- src/main/java/com/billyclub/points/model/PointsEvent.java
+++ src/main/java/com/billyclub/points/model/PointsEvent.java
@@ -6,6 +6,7 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.hateoas.RepresentationModel;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -14,15 +15,18 @@ import java.time.LocalTime;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-
+@EqualsAndHashCode(callSuper = false)
 public class PointsEvent extends RepresentationModel {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "pointsevent_id_seq")
     @SequenceGenerator(name="pointsevent_id_seq", sequenceName = "pointsevent_seq", allocationSize = 1, initialValue = 1000)
     @Column(name="id",updatable = false, nullable = false)
     private long id;
+//    @NotNull(message = "Event date cannot be null")
     private LocalDate eventDate;
+//    @NotNull(message = "Starting time cannot be null")
     private LocalTime startTime;
+//    @NotNull(message = "number of tee times cannot be null")
     private int numOfTimes;
     public PointsEvent(LocalDate date, LocalTime time, int numOfTimes) {
         this.eventDate = date;
